6cb9a970fe561352fa8d7537e529da570f91bd51
Kirub el <Kirubelwondwoson635@gmail.com>
2024-08-15 15:02:35 +0300

    order

--- Routers/OrderRouter.js
+++ Routers/OrderRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const authenticateToken = require('../Middleware/authenticateToken')
 const {CreateOrder, GetOrders, GetMyOrder, EditOrder} = require('../Controller/OrderController')
 
-router.post('/createorder', CreateOrder)
+router.post('/createorder', authenticateToken, CreateOrder)
 router.get('/getorders',authenticateToken, GetOrders)
 router.get('/getmyorder', authenticateToken, GetMyOrder)
 router.patch('/editorder',authenticateToken, EditOrder)
